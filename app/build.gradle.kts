7963069cbb8faf2355ff01aff7a7c9ea73e42b9c
ndriqa <cimilimi1998@gmail.com>
2025-03-29 23:18:36 +0100

    update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "com.ndriqa.cleansudoku"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
