4ab503c8ce05a34b999daf49a651ade2cabde349
ndriqa <cimilimi1998@gmail.com>
2026-07-25 00:20:32 +0200

    chore: bump version code to 4 and version name to 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.ndriqa.cleansudoku"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.1"
+        versionCode = 4
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
